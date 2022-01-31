97bafb968ccd10539f6a783f36eb0fd993c998e3
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-01-31 09:49:16 +0530

    completed the setup for Environment Variable

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const products = require('./product');
 const cors = require('cors');
+
+dotenv.config();
 
 const app = express();
 
@@ -21,4 +24,9 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product);
 });
 
-app.listen(5000, console.log('Server running in port 5000'));
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
