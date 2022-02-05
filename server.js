f8cf21366769650fbc5232cabd004f40ee8dcb8f
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-02-05 19:10:38 +0530

    added notFound and errorHandler express custom middleware created to server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middlewares/errorMiddleware.js';
 
 dotenv.config();
 
@@ -20,9 +21,9 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes);
 
-app.use();
+app.use(notFound);
 
-app.use();
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
