68c74f4d297925f2aeba29901901f20ede798ba6
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-02-05 18:51:24 +0530

    added functionality for GET /api/products and GET /api/products/:id

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -21,7 +21,7 @@ router.get(
 router.get(
   '/:id',
   asyncHandler(async (req, res) => {
-    const product = Product.findById(req.params.id);
+    const product = await Product.findById(req.params.id);
 
     if (product) res.json(product);
     else res.status(404).json({ message: 'Product not found' });
