eca50a0faba52bac60eb8395d06dfcc6296838ec
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-02-19 11:25:59 +0530

    completed controller function getProductById()

--- controllers/productController.js
+++ controllers/productController.js
@@ -10,6 +10,8 @@ const getProducts = asyncHandler(async (req, res) => {
 });
 
 const getProductById = asyncHandler(async (req, res) => {
-  const products = await Product.find({});
-  res.json(products);
+  const product = await Product.findById(req.params.id);
+
+  if (product) res.json(product);
+  else res.status(404).json({ message: 'Product not found' });
 });
