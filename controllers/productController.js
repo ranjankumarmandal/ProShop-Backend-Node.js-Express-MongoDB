54b311a1d9e49adcc1a3d64751060f1c30b87ee4
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-02-19 11:17:30 +0530

    created controller function getProducts()

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,7 @@
 import asyncHandler from 'express-async-handler';
 import Product from '../models/productModel.js';
 
-const router = express.Router();
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({});
+  res.json(products);
+});
