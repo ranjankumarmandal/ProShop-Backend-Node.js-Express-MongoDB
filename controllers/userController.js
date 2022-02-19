b00275664e077b27c3fde24b1ed682c41f368e68
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-02-19 12:03:17 +0530

    started working on userController.js

---
+++ controllers/userController.js
@@ -0,0 +1,10 @@
+import asyncHandler from 'express-async-handler';
+import User from '../models/userModel.js';
+
+// @desc    Fetch all products
+// @route   GET /api/products
+// @access  Public
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({});
+  res.json(products);
+});
