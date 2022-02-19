5133b2550a275c413a26f9eaab621f4cd58f74a1
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-02-19 17:08:30 +0530

    updated file userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,7 +7,7 @@ import User from '../models/userModel.js';
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
-  res.send(email, password);
+  res.send({ email, password });
 });
 
 export { authUser };
