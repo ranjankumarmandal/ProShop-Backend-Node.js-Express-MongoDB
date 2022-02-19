7b799ba4fcef2c460ffe01865c0f38b153d2e349
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-02-19 11:44:46 +0530

    added controller function getProductById() in productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -8,15 +8,6 @@ import {
 const router = express.Router();
 
 router.get('/', getProducts);
-
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-
-    if (product) res.json(product);
-    else res.status(404).json({ message: 'Product not found' });
-  })
-);
+router.get('/:id', getProductById);
 
 export default router;
