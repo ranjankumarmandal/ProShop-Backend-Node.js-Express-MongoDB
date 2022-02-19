38382281aa41cf25248a4c21cd084248e19eb752
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-02-19 11:27:23 +0530

    added comments

--- controllers/productController.js
+++ controllers/productController.js
@@ -9,9 +9,11 @@ const getProducts = asyncHandler(async (req, res) => {
   res.json(products);
 });
 
+// @desc    Fetch single product
+// @route   GET /api/products/:id
+// @access  Public
 const getProductById = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
-
   if (product) res.json(product);
   else res.status(404).json({ message: 'Product not found' });
 });
