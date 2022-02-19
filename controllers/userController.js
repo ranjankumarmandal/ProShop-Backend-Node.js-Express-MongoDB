8b2b0fde17a703978ab7318b13068349798d0757
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-02-19 15:42:50 +0530

    working for controller function authUser()

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,13 @@
 import asyncHandler from 'express-async-handler';
 import User from '../models/userModel.js';
 
-// @desc    Fetch all products
-// @route   GET /api/products
+// @desc    Auth user & get token
+// @route   POST /api/users/login
 // @access  Public
-const getProducts = asyncHandler(async (req, res) => {
-  const products = await Product.find({});
-  res.json(products);
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  res.send(email, password);
 });
+
+export { authUser };
