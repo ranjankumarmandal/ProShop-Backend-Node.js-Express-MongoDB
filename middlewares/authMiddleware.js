925c053f3e7cc0394062a299da536b92a5bbc89b
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-02-20 15:42:55 +0530

    completed try in protech() in authMiddleware.js

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken';
 import User from '../models/userModel.js';
+import asyncHandler from 'express-async-handler';
 
 const protect = async (req, res, next) => {
   let token;
@@ -8,10 +9,20 @@ const protect = async (req, res, next) => {
     req.headers.authorization.startsWith('Bearer')
   ) {
     console.log('token found');
+    try {
+      const decoded = jwt.verify(
+        req.headers.authorization.split(' ')[1],
+        process.env.JWT_SECRET
+      );
+
+      req.user = User.findById(decoded.id).select('-password');
+      console.log(req.user);
+      next();
+    } catch (error) {}
   } else {
+    console.log('not authorized');
     res.status(401).json({ message: 'Not authorized, no token' });
   }
-  next();
 };
 
 export { protect };
