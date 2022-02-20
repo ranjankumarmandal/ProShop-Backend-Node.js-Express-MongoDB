9d02e553c6bcf5f234f1b411810a400b2f3840fd
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-02-20 15:58:42 +0530

    updated authMiddleware.js

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -3,7 +3,6 @@ import User from '../models/userModel.js';
 import asyncHandler from 'express-async-handler';
 
 const protect = async (req, res, next) => {
-  let token;
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith('Bearer')
