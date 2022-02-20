d3a4ae6e37ffaa5a70c05eff704de8247bcd5612
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-02-20 14:41:00 +0530

    added express middleware protect() in between path and getUserProfile from userController.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,6 +5,6 @@ import { protect } from '../middlewares/authMiddleware.js';
 const router = express.Router();
 
 router.post('/login', authUser);
-router.get('/profile', getUserProfile);
+router.get('/profile', protect, getUserProfile);
 
 export default router;
