0eba3aee4fac477e0b5ca290451302b38d309e93
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-02-20 22:54:49 +0530

    started working on registerUser() in userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,6 +6,26 @@ import jwt from 'jsonwebtoken';
 // @desc    Auth user & get token
 // @route   POST /api/users/login
 // @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  const id = user._id; // user id from mongoDB for jwt.sign()
+
+  if (user && bcrypt.compare(user.password, password)) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '1h' }),
+    });
+  } else res.status(401).json({ message: 'Invalid email or password' });
+});
+
+// @desc    Auth user & get token
+// @route   POST /api/users/login
+// @access  Public
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
