65e8b4efc1a9f20a058e593274ab0aef8a9b9b67
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-02-20 14:38:46 +0530

    started working on protect() in authMiddleware.js

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,2 +1,10 @@
 import jwt from 'jsonwebtoken';
-import User from '../models/userModel';
+import User from '../models/userModel.js';
+
+const protect = async (req, res, next) => {
+  let token = 1;
+  console.log(req.header.authorization);
+  next();
+};
+
+export { protect };
