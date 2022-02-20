c76d31072a327cd2301052f2830dee3f926f5d4f
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-02-20 12:52:17 +0530

    imported jwt in userController for authentication and authorization purpose

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 import asyncHandler from 'express-async-handler';
 import User from '../models/userModel.js';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 // @desc    Auth user & get token
 // @route   POST /api/users/login
