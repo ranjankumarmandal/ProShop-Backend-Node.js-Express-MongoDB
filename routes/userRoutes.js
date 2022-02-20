199f6ce86a27d5182e7831d0ac60a2503f00f607
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-02-20 23:30:02 +0530

    import registerUser in userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,9 @@
 import express from 'express';
-import { authUser, getUserProfile } from '../controllers/userController.js';
+import {
+  registerUser,
+  authUser,
+  getUserProfile,
+} from '../controllers/userController.js';
 import { protect } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
