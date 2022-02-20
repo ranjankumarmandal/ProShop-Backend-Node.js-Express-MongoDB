e89ccc240acc30df15b229d27eb1c57f0f9c71d0
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-02-20 23:08:15 +0530

    if user not already in mongoDB, create a new user with name, email and password

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,6 +10,9 @@ const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
   const userExists = await User.findOne({ email });
+  if (userExists) res.status(400).json({ message: 'User already exists' });
+
+  const user = await User.create({ name, email, password });
 });
 
 // @desc    Auth user & get token
