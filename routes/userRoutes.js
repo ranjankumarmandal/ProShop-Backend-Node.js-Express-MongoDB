8a6f17db02346be94d4b9e0ae2820b3508eef267
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-02-27 15:30:59 +0530

    added updateUserProfile controller function with protect in file userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -12,5 +12,6 @@ const router = express.Router();
 router.post('/', registerUser);
 router.post('/login', authUser);
 router.get('/profile', protect, getUserProfile);
+router.put('/profile', protect, updateUserProfile);
 
 export default router;
