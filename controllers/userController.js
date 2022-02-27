5240975f61d18d48b3171dad3a088ba3ca25aadb
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-02-27 15:19:26 +0530

    completed updateUserProfile route in userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -55,12 +55,14 @@ const authUser = asyncHandler(async (req, res) => {
 // @route   GET /api/users/profile
 // @access  Private
 const getUserProfile = asyncHandler(async (req, res) => {
-  if (req.user) {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
     res.json({
-      _id: req.user._id,
-      name: req.user.name,
-      email: req.user.email,
-      isAdmin: req.user.isAdmin,
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
     });
   } else {
     res.status(404).json({ message: 'User not found' });
@@ -71,12 +73,22 @@ const getUserProfile = asyncHandler(async (req, res) => {
 // @route   PUT /api/users/profile
 // @access  Private
 const updateUserProfile = asyncHandler(async (req, res) => {
-  if (req.user) {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) user.password = req.body.password;
+
+    const updatedUser = await user.save();
+    const id = updatedUser._id;
+
     res.json({
-      _id: req.user._id,
-      name: req.user.name,
-      email: req.user.email,
-      isAdmin: req.user.isAdmin,
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '1h' }),
     });
   } else {
     res.status(404).json({ message: 'User not found' });
