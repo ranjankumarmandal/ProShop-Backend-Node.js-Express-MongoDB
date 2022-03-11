e37dac28eee2952a4c96faa9b7815d8262baa1b4
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-03-11 23:35:43 +0530

    started working on orderController.js

---
+++ controllers/orderController.js
@@ -0,0 +1,10 @@
+import asyncHandler from 'express-async-handler';
+import Order from '../models/orderModel';
+
+// @desc    Fetch all products
+// @route   GET /api/products
+// @access  Public
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({});
+  res.json(products);
+});
