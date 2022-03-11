bc79023ff280ff8c976559184090810adc43cca6
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-03-11 23:44:50 +0530

    started working on that addOrderItems() in orderController.js

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -4,7 +4,4 @@ import Order from '../models/orderModel';
 // @desc    Create new order
 // @route   POST /api/orders
 // @access  Private
-const getProducts = asyncHandler(async (req, res) => {
-  const products = await Product.find({});
-  res.json(products);
-});
+const addOrderItems = asyncHandler(async (req, res) => {});
