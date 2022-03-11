c14640ef435087cd8444759cfd4652c59796bf5e
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-03-11 23:47:06 +0530

    destructured the all related data coming from req.body

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -4,4 +4,14 @@ import Order from '../models/orderModel';
 // @desc    Create new order
 // @route   POST /api/orders
 // @access  Private
-const addOrderItems = asyncHandler(async (req, res) => {});
+const addOrderItems = asyncHandler(async (req, res) => {
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+});
