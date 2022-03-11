20210438be8b0946c78fcb05f86a0bbe75f5fbe4
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-03-11 23:42:33 +0530

    access will be private

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -3,7 +3,7 @@ import Order from '../models/orderModel';
 
 // @desc    Create new order
 // @route   POST /api/orders
-// @access  Public
+// @access  Private
 const getProducts = asyncHandler(async (req, res) => {
   const products = await Product.find({});
   res.json(products);
