75f86467e616f625c1427376eee97407d5ec4df6
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-03-19 10:18:48 +0530

    import getOrderById in file orderRoutes.js

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addOrderItems } from '../controllers/orderController.js';
+import { addOrderItems, getOrderById } from '../controllers/orderController.js';
 import { protect } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
