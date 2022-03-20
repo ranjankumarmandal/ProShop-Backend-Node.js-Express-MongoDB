50feef07c6bd88682b445045cc34ff3a5c0b6aa9
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-03-20 15:10:59 +0530

    started working show order on profile in orderController.js

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -69,4 +69,25 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
   } else res.status(404).json({ message: 'Order not found' });
 });
 
+// @desc    Get logged in user order
+// @route   PUT /api/orders/:id/pay
+// @access  Private
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    };
+
+    const updatedOrder = await order.save();
+    res.json(updatedOrder);
+  } else res.status(404).json({ message: 'Order not found' });
+});
+
 export { addOrderItems, getOrderById, updateOrderToPaid };
