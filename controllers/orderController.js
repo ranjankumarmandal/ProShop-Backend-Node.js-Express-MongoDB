2d383fe21046e6efac078e8989f499c5b269954c
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-03-20 23:06:15 +0530

    export getMyOrders in file orderController.js

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -77,4 +77,4 @@ const getMyOrders = asyncHandler(async (req, res) => {
   res.json(orders);
 });
 
-export { addOrderItems, getOrderById, updateOrderToPaid };
+export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders };
